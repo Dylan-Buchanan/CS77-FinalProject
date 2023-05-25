d96afd7c0fb427f4d44e75807715ef5c0076c1ee
Andrew Ellis <121977420+andrewell13@users.noreply.github.com>
2023-05-25 16:21:50 -0400

    smooth movement

--- game.js
+++ game.js
@@ -33,7 +33,7 @@ var BlackFragmentSource = `
     }
 `;
 
-var speed = 0.2;
+var speed = 0.02;
 var skinniest = 0.4;
 
 function moveRight()
@@ -90,6 +90,7 @@ function moveDown()
 
 function moveLeft()
 {
+    console.log("here");
     CubePositions[0] -= speed;
     CubePositions[9] -= speed;
     CubePositions[12] -= speed;
@@ -139,113 +140,97 @@ var upped = false;
 var downed = false;
 var widened = false;
 
-var keyA = false;
+const keyStates = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+}
 
 document.addEventListener('keyup', (event) => {
-    if (event.key === 'a' || event.key === 'A') {
-        keyA = false;
-        console.log(keyA);
+    const key = event.key;
+  
+    if (key in keyStates) {
+        keyStates[key] = false;
+        // checkKeyStates();
     }
 });
+
 
 document.addEventListener('keydown', (event) => {
 
-      // w and a
-    if (event.key === 'w' && keyA == true)  {
-        // Perform an action when the Enter key is pressed
-        console.log(keyA);
-        console.log("Pressed");
-        height += speed;
-        trans -= speed;
-        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    const key = event.key;
+    console.log(key);
+  
+    if (key in keyStates) {
+        keyStates[key] = true;
+        // checkKeyStates();
     }
-    // w and d
-    // w and left
-    // w and right
-    // w and up
-    // w and down
-
-    // a and s
-    // a and up
-    // a and down
-    // a and left
-    // a and right
-
-    // d and s
-    // d and up
-    // d and down
-    // d and left 
-    // d and right
-
-    // s and up
-    // s and left
-    // s and right
-    // s and down
-
-    // up and left
-    // up and right
-    
-    // left and down
-
-    // right and down
-
-    // Check if the pressed key is the one you're interested in
-    if (event.key === 'a' || event.key === 'A') {
-      // Perform an action when the Enter key is pressed
-        console.log("Pressed");
-        keyA = true;
-        trans -= speed;
-        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'd' || event.key === 'D') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          trans += speed;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'w' || event.key === 'W') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          height += speed;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 's' || event.key === 'S') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          height = Math.max(height -= speed, -1.5);
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'ArrowUp') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          moveUp();
-          upped = true;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'ArrowDown') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          moveDown();
-          downed = true;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'ArrowRight') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          moveSkinny();
-          widened = true;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-    if (event.key === 'ArrowLeft') {
-        // Perform an action when the Enter key is pressed
-          console.log("Pressed");
-          moveLeft();
-          widened = true;
-          cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
-    }
-
-    
 });
+
+// Function to check key states and perform actions
+function checkKeyStates() {
+    const { w, a, s, d, ArrowUp, ArrowDown, ArrowLeft, ArrowRight } = keyStates;
+    
+    // Check the state of all keys
+    if (w) {
+      // Perform action when W is pressed
+        height += speed;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+    
+    if (a) {
+      // Perform action when A is pressed
+        trans -= speed;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+    
+    if (s) {
+      // Perform action when S is pressed
+        height = Math.max(height -= speed, -1.5);
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+    
+    if (d) {
+      // Perform action when D is pressed
+        trans += speed;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+    if (ArrowUp) {
+        // Perform action when up is pressed
+        console.log("Here");
+        moveUp();
+        upped = true;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+      
+    if (ArrowDown) {
+    // Perform action when down is pressed
+        moveDown();
+        downed = true;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+      
+    if (ArrowLeft) {
+        console.log("Here 3");
+    // Perform action when left is pressed
+        moveLeft();
+        widened = true;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+      
+    if (ArrowRight) {
+    // Perform action when right is pressed
+        moveSkinny();
+        widened = true;
+        cubeModel = Matrix.translate(trans, height, 0).multiply(Matrix.scale(0.5, 0.5, 0.5));
+    }
+
+  }
 
 var previousTime = performance.now();
 
@@ -257,6 +242,9 @@ Game.prototype.render = function(gl, w, h)
     var projection = Matrix.perspective(45, w/h, 0.1, 100);    
     var view = Matrix.rotate(-this.yaw, 0, 1, 0).multiply(Matrix.rotate(-this.pitch, 1, 0, 0)).multiply(Matrix.translate(0, 0, 5)).inverse();
     var rotation = Matrix.rotate(Date.now()/25, 0, 1, 0);
+
+    // check for the key states constantly for smooth movement
+    checkKeyStates();
 
     if (upped || downed || widened) {
         this.cubeMesh = new ShadedTriangleMesh(gl, CubePositions, CubeNormals, CubeIndices, BlackVertexSource, BlackFragmentSource);
